b2703bb5055f0d6516c27d5eb422b49856ded225
Ariel Martinez <ariel.mtz85@gmail.com>
2025-06-30 15:45:55 -0300

    test2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://test-render-backend-h55a.onrender.com/'  // URL local del backend
 };
+
+///test-render-backend-h55a.onrender.com/api/tasks
