8ff1ec026b150453096c8d23631cfbba4c4fe27b
Ariel Martinez <ariel.mtz85@gmail.com>
2025-06-30 11:41:59 -0300

    agregando url del backend

--- src/environments/envirnment.prod.ts
+++ src/environments/envirnment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend-en-render.onrender.com/api'  // Reemplaza con tu URL en Render
+  apiUrl: 'https://test-render-backend-h55a.onrender.com'  // Reemplaza con tu URL en Render
 };
